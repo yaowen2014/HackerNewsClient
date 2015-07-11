d1c70cbc259fbda7ebe5556d014545b75df02f9c
yaowen.zheng <johnazheng@gmail.com>
2015-07-11 20:11:24 +0800

    simply implement load more by scroll near bottom

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,14 @@
 (function(){
     var Page = function(){
+        var self = this;
         this.topStoriesList = new App.ViewModels.List(App.ViewModels.Story, "topstories", {
             needInit: true
+        });
+
+        $(window).scroll(function(){
+            if (window.scrollY > document.body.clientHeight - window.innerHeight - 50) {
+                self.topStoriesList.loadMore();
+            }
         });
     }
 
